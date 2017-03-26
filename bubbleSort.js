8062553f0af4a698b22508ce014e541ae4683d7d
Sreeram Padmanabhan <sreeram.com@gmail.com>
2017-03-26 12:11:09 +0100

    updates

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,14 +1,13 @@
-var { swapArrayElement } = require('./utils');
+const { swapArrayElement } = require('./utils');
 const bubbleSort = (data, func) => {
-
-	for (var i=data.length-1; i > 0; i--) {
-		for(var j=0; j<i; j++) {
-			func(data[j], data[j+1]) ? swapArrayElement(data, j) : false;
+let k=0;
+	for (let i=data.length-1; i > 0; i--) { // let length be 5
+		for(let j=0; j<i; j++) { //when i is 4, loop through 0 to 3
+			func(data[j], data[j+1]) ? swapArrayElements(data, j) : false;
 		}
 	}
 	return data;
-	
 };
 
-const sort = (a, b) => !(b-a > 0);
-console.log(bubbleSort([4, 5, 1, 3, 9, 0, 2, 18, 199], sort));
+const sort = (a, b) => b - a > 0;
+process.stdout.write(bubbleSort([4, 5, 1, 3, 9, 0, 2, 18, 199], sort).toString());
