00fc8db805643186d7da8cdf5b91b130378adb79
Sreeram Padmanabhan <sreeram.com@gmail.com>
2017-03-29 00:24:48 +0100

    removeSingleList

--- linked_list/singly_linked_list.js
+++ linked_list/singly_linked_list.js
@@ -17,10 +17,21 @@ class Sll {
             current.next = node;
         }
     }
+    remove(val) {
+        let current = this._root;
+        while(current.next) {
+            if(current.next.val !== val)
+                current = current.next;
+            else 
+                current.next = current.next.next;
+        }
+    }
 }
 
 var sll = new Sll();
 sll.push(1);
 sll.push(2);
-
+sll.push(3);
+console.log(sll._root);
+sll.remove(2);
 console.log(sll._root);
